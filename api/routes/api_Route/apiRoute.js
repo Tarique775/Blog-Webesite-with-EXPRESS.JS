fad46785615ebedbad3f52211fc9246e6f8a129e
Tarikul Islam <islam.tarique4329@gmail.com>
2021-06-22 23:53:51 +0600

    added comment feature

--- api/routes/api_Route/apiRoute.js
+++ api/routes/api_Route/apiRoute.js
@@ -9,8 +9,8 @@ const {
 
 const router = express.Router();
 
-router.post('/post/comment/:postId', isAuth, postCommentController);
+router.post('/post/comments/:postId', isAuth, postCommentController);
 
-router.post('/post/replies/:commentId', isAuth, postRepliesController);
+router.post('/post/comments/replies/:commentId', isAuth, postRepliesController);
 
 module.exports = router;
