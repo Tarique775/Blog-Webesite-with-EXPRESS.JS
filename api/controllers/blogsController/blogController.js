f1783d6495aaa42e9e3907b4ddb40b0387d7a970
tarikul islam <islam.tarique4329@gmail.com>
2021-07-05 23:10:54 +0600

    blog page view

--- api/controllers/blogsController/blogController.js
+++ api/controllers/blogsController/blogController.js
@@ -1,9 +1,18 @@
+const Post = require('../../models/post');
+
 const controller = {};
 
-controller.getBlogController = (req, res, next) => {
-    res.render('pages/user/blogs', {
-        filter: 'latest',
-    });
+controller.getBlogController = async (req, res, next) => {
+    const filter = req.query.filter || 'latest';
+    try {
+        const posts = await Post.find();
+        res.render('pages/user/blogs', {
+            filter,
+            posts,
+        });
+    } catch (e) {
+        next(e);
+    }
 };
 
 module.exports = controller;
--- api/middleware/locals.js
+++ api/middleware/locals.js
@@ -1,5 +1,16 @@
+const cheerio = require('cheerio');
+const moment = require('moment');
+
 module.exports = () => (req, res, next) => {
     res.locals.token = req.token;
     res.locals.user = req.user;
+    res.locals.truncate = (html) => {
+        const node = cheerio.load(html);
+        let text = node.text();
+
+        text = text.replace(/(\r\n|\n|\r)/gm, '');
+        return text;
+    };
+    res.locals.moment = (time) => moment(time).fromNow();
     next();
 };
