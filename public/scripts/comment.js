5da650c90503a45fb674102791b0d8ab4047cdd6
tarikul islam <islam.tarique4329@gmail.com>
2021-08-20 17:11:41 +0600

    final touch

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -1,5 +1,5 @@
-const socket = io('http://localhost:8080');
-
+// const socket = io('http://localhost:5000');
+const socket = io('https://web-blog-bd.herokuapp.com');
 const comment = document.getElementById('comment');
 const commentHolder = document.getElementById('comment-holder');
 
